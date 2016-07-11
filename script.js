aa54ee924b1f53cc6919ebcebfc3f39c0cf1d9e7
warnsinnig <marc@obieglo.net>
2016-07-11 11:02:10 +1000

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So this is just a test, don't get too excited.')
                 .then(() => 'speak');
         }
     },
